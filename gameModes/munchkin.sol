edeadf6bd5bcc6e152fdaf10245c86e9fec50248
hung pham <cogangsekhongthua1994@gmail.com>
2021-04-01 17:10:05 +0700

    zombiecontract

---
+++ gameModes/munchkin.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.5.11;
+
+import "../ZBGameMode.sol";
+
+contract Munchkin is ZBGameMode  {
+
+    function beforeMatchStart(bytes serializedGameState) external {
+
+        GameState memory gameState;
+        gameState.init(serializedGameState);
+
+        ZBSerializer.SerializedGameStateChanges memory changes;
+        changes.init();
+
+        for (uint i = 0; i < gameState.playerStates[i].length; i++) {
+            CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
+            uint cardCount = 0;
+
+            for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
+                if (isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    cardCount++;
+                }
+            }
+        }
+
+        changes.changePlayerCardsInDeck(Players(i), newCards, cardCount);
+        changes.emitStateChange();
+}
+
+    function isLegalCard(CardInstance card) internal view returns(bool) {
+        return (card.gooCost <= 2);
+    }
+
+}
